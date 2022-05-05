258ca7cd3f14585b3996b24b5e0ce842126dacca
shivam140 <83869481+shivam140@users.noreply.github.com>
2022-05-05 10:33:10 +0530

    Add files via upload

---
+++ spring-boot-chat-app/spring-boot-chat-app/src/main/java/com/shivam/springbootchatapp/SpringBootChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.shivam.springbootchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatAppApplication.class, args);
+	}
+
+}
